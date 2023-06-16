e3b551d61027b3b1bf2fab0e19eb2905551ec105
Бурдейный Евгений <anahielitl@gmail.com>
2023-06-16 23:01:04 +0300

    Games

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.AccessibleName = "";
+            this.progressBar1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.progressBar1.Location = new System.Drawing.Point(20, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(211, 17);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lime;
+            this.textBox1.Enabled = false;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(237, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 397);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        // размеры лабиринта в ячейках 16х16 пикселей
+        int columns = 20;
+        int rows = 22;
+
+        int pictureSize = 16; // ширина и высота одной ячейки
+
+        Labirint l; // ссылка на логику всего происходящего в лабиринте
+
+        public Form1()
+        {
+            InitializeComponent();
+            Options();
+            StartGame();
+        }
+
+        public void Options()
+        {
+            Text = "Maze";
+            BackColor = Color.FromArgb(255, 92, 118, 137);
+
+            // размеры клиентской области формы (того участка, на котором размещаются ЭУ)
+            ClientSize = new Size(columns * pictureSize, rows * pictureSize);
+
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        public void StartGame()
+        {
+            l = new Labirint(this, columns, rows);
+            progressBar1.Value = 100;
+            textBox1.Text = $"{l.helthPoint} HP";
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)//добавлено управление WASD
+            {
+                // проверка на то, свободна ли ячейка справа
+                if (l.objects[l.CharacterPositionY, l.CharacterPositionX + 1].type ==
+                    MazeObject.MazeObjectType.HALL) // проверяем ячейку правее на 1 позицию, является ли она коридором
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                //проверка на медаль
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX + 1].type ==
+                    MazeObject.MazeObjectType.MEDAL) && (l.countMedals != l.createMedals)) // проверяем ячейку правее на 1 позицию, является ли она Medal
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+
+                    l.PointUp();
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                // enemy
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX + 1].type ==
+                    MazeObject.MazeObjectType.ENEMY))
+                {
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                    {
+                        MessageBox.Show("Потрачено", "You LOSE!!!", MessageBoxButtons.OK);
+                    }
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                        return;
+                    l.helthPoint -= l.RandDamage();//полученый урон отнимается от хп
+                }
+                //healing
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX + 1].type ==
+                    MazeObject.MazeObjectType.HELTH) && l.helthPoint != 100)
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.helthPoint += 100 * 0.05;//полученый урон отнимается от хп
+                }
+                //проверка на выход
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX + 1].type ==
+                    MazeObject.MazeObjectType.EXIT))
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                }
+            }
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                if (l.objects[l.CharacterPositionY, l.CharacterPositionX - 1].type ==
+                   MazeObject.MazeObjectType.HALL) // проверяем ячейку левее на 1 позицию, является ли она коридором
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                //медаль?
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX - 1].type ==
+                    MazeObject.MazeObjectType.MEDAL) && (l.countMedals != l.createMedals)) // проверяем ячейку правее на 1 позицию, является ли она Medal
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+
+                    l.PointUp();
+
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                //enemy
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX - 1].type ==
+                    MazeObject.MazeObjectType.ENEMY))
+                {
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                    {
+                        MessageBox.Show("Потрачено", "You LOSE!!!", MessageBoxButtons.OK);
+                    }
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                        return;
+                    l.helthPoint -= l.RandDamage();//полученый урон отнимается от хп
+                }
+                //healing
+                else if ((l.objects[l.CharacterPositionY, l.CharacterPositionX - 1].type ==
+                   MazeObject.MazeObjectType.HELTH) && l.helthPoint < 100)
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionX--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.helthPoint += 100 * 0.05;//полученый урон отнимается от хп
+                }
+            }
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (l.objects[l.CharacterPositionY + 1, l.CharacterPositionX].type ==
+                                   MazeObject.MazeObjectType.HALL) // проверяем ячейку снизу на 1 позицию, является ли она коридором
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                //медаль?
+                else if ((l.objects[l.CharacterPositionY + 1, l.CharacterPositionX].type ==
+                    MazeObject.MazeObjectType.MEDAL) && (l.countMedals != l.createMedals)) // проверяем ячейку правее на 1 позицию, является ли она Medal
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+
+                    l.PointUp();
+
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                // enemy
+                else if ((l.objects[l.CharacterPositionY + 1, l.CharacterPositionX].type ==
+                    MazeObject.MazeObjectType.ENEMY))
+                {
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                    {
+                        MessageBox.Show("Потрачено", "You LOSE!!!", MessageBoxButtons.OK);
+                    }
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                        return;
+                    l.helthPoint -= l.RandDamage();//полученый урон отнимается от хп
+
+                }
+                //healing
+                else if ((l.objects[l.CharacterPositionY + 1, l.CharacterPositionX].type ==
+                   MazeObject.MazeObjectType.HELTH) && l.helthPoint < 100)
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY++;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.helthPoint += 100 * 0.05;//полученый урон отнимается от хп
+                }
+            }
+            else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                if (l.objects[l.CharacterPositionY - 1, l.CharacterPositionX].type ==
+                                   MazeObject.MazeObjectType.HALL) // проверяем ячейку вверху на 1 позицию, является ли она коридором
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                //медаль?
+                else if ((l.objects[l.CharacterPositionY - 1, l.CharacterPositionX].type ==
+                    MazeObject.MazeObjectType.MEDAL) && (l.countMedals != l.createMedals)) // проверяем ячейку правее на 1 позицию, является ли она Medal
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+
+                    l.PointUp();
+
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+                }
+                // enemy
+                else if ((l.objects[l.CharacterPositionY - 1, l.CharacterPositionX].type ==
+                    MazeObject.MazeObjectType.ENEMY))
+                {
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                    {
+                        MessageBox.Show("Потрачено", "You LOSE!!!", MessageBoxButtons.OK);
+                    }
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    if ((l.helthPoint - l.RandDamage()) <= 0)
+                        return;
+                    l.helthPoint -= l.RandDamage();//полученый урон отнимается от хп
+
+                }
+                //healing
+                else if ((l.objects[l.CharacterPositionY - 1, l.CharacterPositionX].type ==
+                   MazeObject.MazeObjectType.HELTH) && (l.helthPoint < 100))
+                {
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.CharacterPositionY--;
+
+                    l.objects[l.CharacterPositionY, l.CharacterPositionX].texture = MazeObject.images[4]; // character
+                    l.images[l.CharacterPositionY, l.CharacterPositionX].BackgroundImage = l.objects[l.CharacterPositionY, l.CharacterPositionX].texture;
+
+                    l.helthPoint += 100 * 0.05;//полученый урон отнимается от хп
+                }
+            }
+            if ((l.CharacterPositionX == l.ExitPositionX && l.ExitPositionY == l.ExitPositionY) || (l.createMedals == l.countMedals))
+            {
+                MessageBox.Show("Успех", "You WIN!!!", MessageBoxButtons.OK);
+            }
+            var t = Math.Round(l.helthPoint, 1);
+            textBox1.Text = $"{t} HP";
+            progressBar1.Value = (int)(l.helthPoint);
+        }
+    }
+}
